901fc39b90308015dfdd0d8bc95267a3d8759867
syerlinadya <syerlitrinadya14@gmail.com>
2021-10-21 15:51:44 +0700

    menambahkan navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+
+import Navigasi  from "./Component/Navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigasi/>
     </div>
   );
 }
